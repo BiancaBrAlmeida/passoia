9b24ce4719e4ae72861dd5a2c8c2b14cf1d6176d
BiancaBrAlmeida <biancabr.almeida@gmail.com>
2025-11-12 19:21:21 -0300

    configuração inicial

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <img src="" alt="" />
+      <nav>
+        <ul>
+          <li>LOOKS</li>
+          <li>LANÇAMENTOS</li>
+          <li>NOVIDADES</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
