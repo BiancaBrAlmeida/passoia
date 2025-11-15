f9e23d0ea9eee267921c7159ca084a5d8cec7ad7
BiancaBrAlmeida <biancabr.almeida@gmail.com>
2025-11-15 01:06:40 -0300

    começo da modificação da header - Mobile First

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,28 @@
+// import { useState } from "react";
+import "./header.scss";
+import logo from "../../assets/logo.png";
+import icon from "../../assets/icon.png";
+
 function Header() {
+  // const [aberto, setAberto] = useState(false);
+
   return (
     <header>
-      <img src="" alt="" />
+      <img src={logo} alt="Logo" />
       <nav>
         <ul>
-          <li>LOOKS</li>
-          <li>LANÇAMENTOS</li>
-          <li>NOVIDADES</li>
+          <li>
+            <a href="">LOOKS</a>
+          </li>
+          <li>
+            <a href="">LANÇAMENTOS</a>
+          </li>
+          <li>
+            <a href="">NOVIDADES</a>
+          </li>
         </ul>
       </nav>
+      <img src={icon} alt="Icon menu" />
     </header>
   );
 }
---
+++ src/components/Lancamentos/index.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import azul from "../../assets/azul.png";
+import vermelho from "../../assets/vermelho.png";
+import marrom from "../../assets/marrom.png";
+import nude from "../../assets/nude.png";
+
+function Lancamentos() {
+  // essa linha serve para atualizar estados
+  const [cor, setCor] = useState(vermelho);
+  return (
+    <>
+      <h2>APROVEITE OS LANÇAMENTOS</h2>
+      <img src={cor} alt="" />
+      <button onClick={() => setCor(vermelho)}>Vermelho</button>
+      <button onClick={() => setCor(azul)}>Azul</button>
+      <button onClick={() => setCor(marrom)}>Marrom</button>
+      <button onClick={() => setCor(nude)}>Nude</button>
+    </>
+  );
+}
+export default Lancamentos;
