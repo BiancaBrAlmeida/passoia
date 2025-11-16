48297fb06781eae5a924e3cb1fcbbcf2cdad5d71
BiancaBrAlmeida <biancabr.almeida@gmail.com>
2025-11-16 03:12:00 -0300

    início do menu sandwich responsivo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,29 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./header.scss";
-import logo from "../../assets/logo.png";
-import icon from "../../assets/icon.png";
+import logo from "../../assets/logo.svg";
 
 function Header() {
-  // const [aberto, setAberto] = useState(false);
+  const [aberto, setAberto] = useState(false);
+  const toggleMenu = () => {
+    setAberto(!aberto);
+  };
 
   return (
     <header>
       <img src={logo} alt="Logo" />
-      <nav>
+      <nav className={`${"menu"} ${aberto ? "mostrar" : ""}`}>
+        {/* <button className="btn_menu">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-x"
+            viewBox="0 0 16 16"
+          >
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+          </svg>
+        </button> */}
         <ul>
           <li>
             <a href="">LOOKS</a>
@@ -18,11 +32,25 @@ function Header() {
             <a href="">LANÇAMENTOS</a>
           </li>
           <li>
-            <a href="">NOVIDADES</a>
+            <a href="">NOVIDADE</a>
           </li>
         </ul>
       </nav>
-      <img src={icon} alt="Icon menu" />
+      <button className="btn_menu" onClick={toggleMenu}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          fill="currentColor"
+          class="bi bi-list"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+          />
+        </svg>
+      </button>
     </header>
   );
 }
