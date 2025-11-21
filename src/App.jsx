f1424804ed91784320052108b9c940ffbe0770f7
BiancaBrAlmeida <biancabr.almeida@gmail.com>
2025-11-21 01:30:28 -0300

    adição do footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Banner from "./components/Banner";
 import Looks from "./components/Looks";
 import Lancamentos from "./components/Lancamentos";
 import Novidades from "./components/Novidades";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Looks />
       <Lancamentos />
       <Novidades />
+      <Footer />
     </>
   );
 }
